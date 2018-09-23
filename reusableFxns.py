bfb8cd699d07adec2e6bc9a9ed74b8479de09973
unknown <14913130+phillram@users.noreply.github.com>
2018-09-23 12:18:08 -0700

    Updated pathing for counting file. Removed pycache.

--- reusableFxns.py
+++ reusableFxns.py
@@ -2,16 +2,24 @@
 # Functions to be reused across other files
 ####################################################################
 
+###################################################################
+# Imports that are good to use
+# Predominately to find pathing between Windows and Unix
+###################################################################
+from pathlib import Path
 
 ###################################################################
 # This opens a file to increment the number
 ###################################################################
-def getNumber(filename="countFile.txt"):
+
+countFilePath = Path('saucelabs-simple-python\countFile.txt')
+
+def getNumber(filename = countFilePath):
     with open(filename, "r+") as f:
         val = int(f.read() or 0) + 1
         f.seek(0)
         f.truncate()
         f.write(str(val))
         f.close()
-        print ('This is test Run: ' + str(val))
+        print ('This is test run: ' + str(val))
         return str(val)
