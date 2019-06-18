265489adbb3b9e165721eb0eefb3a51b092b2579
Phill <phillip.ramirez@saucelabs.com>
2019-06-18 10:30:28 -0700

    Added URL endpoints to cheatsheet

--- CheatSheet.py
+++ CheatSheet.py
@@ -136,3 +136,15 @@ driver.quit()
 ###################################################################
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+###################################################################
+# This is a list of the data center URLs.
+# Mainly for reference, but you're welcome to use the variables
+###################################################################
+us_west_dc = 'ondemand.saucelabs.com/wd/hub'
+us_east_dc = 'ondemand.eu-central-1.saucelabs.com/wd/hub'
+eu_west_dc = 'ondemand.us-east-1.saucelabs.com/wd/hub'
+
+
+
+
