97a0037ec868a83ff989f58f35ca19192e241db8
phillram <phill.ramirez@gmail.com>
2020-01-30 14:55:48 -0800

    Added RDC sample for Unified Platform. Renamed both Sauce Appium to denote real or virtual devices

---
+++ JSONWireProtocol/skeletonAppiumRDC-SauceLabs.py
@@ -0,0 +1,145 @@
+####################################################################
+# Skeleton for Appium tests on Sauce Labs Real Devices - Unified Platform
+# This is currently in BETA and will only work for private devices
+####################################################################
+
+###################################################################
+# Imports that are good to use
+###################################################################
+from appium import webdriver
+from time import sleep
+import sys
+import os
+import time
+from reusableFxns import *
+import requests
+androidTest = False
+iosTest = False
+
+###################################################################
+# Selenium with Python doesn't like using HTTPS correctly
+# and displays a warning that it uses Unverified HTTPS request
+# The following disables that warning to clear the clutter
+# But I should find a way to do the proper requests
+###################################################################
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+###################################################################
+# Choose if you want Android of iOS capabilities
+# Uncomment one of those lines
+###################################################################
+# androidTest = True
+# iosTest = True
+
+###################################################################
+# Select Data Center
+# Set region to 'US' or 'EU'
+# Test will default to 'US' if left blank or set to any other than 'US' or 'EU'
+###################################################################
+region = 'US'
+
+###################################################################
+# Common parameters (desired capabilities)
+# For Test Object tests
+###################################################################
+projectParameters = {
+    'tags':['Case', 'NUM',],
+    'appiumVersion': '1.16.0',
+    'name': 'Run: ' + getNumber(),
+}
+
+androidParameters = { # Define Android parameters here
+    'deviceName' : 'Samsung.*',
+    'platformVersion' : '9',
+    'browserName' : 'Chrome',
+    'deviceOrientation' : 'portrait',
+    'platformName' : 'Android',
+}
+
+iosParameters = { # Define iOS Parameters here
+    'deviceName' : 'iPhone X',
+    'deviceOrientation' : 'portrait',
+    'browserName' : 'safari',
+    'platformVersion' : '12',
+    'platformName' : 'iOS',
+    # 'nativeWebTap': True, # iOS only capability.
+}
+
+###################################################################
+# Merge parameters into a single capability dictionary
+###################################################################
+sauceParameters = {}
+sauceParameters.update(projectParameters)
+if androidTest != True and iosTest != True:
+    print('You need to specify a platform to test on!')
+    sys.exit()
+elif androidTest == True and iosTest == True:
+    print('Don\'t be greedy! Only choose one platform!')
+    sys.exit()
+elif androidTest:
+    sauceParameters.update(androidParameters)
+elif iosTest:
+    sauceParameters.update(iosParameters)
+
+# This concatenates the tags key above to add the build parameter
+sauceParameters.update({'build': '-'.join(sauceParameters.get('tags'))})
+
+###################################################################
+# Connect to Sauce Labs
+###################################################################
+try:
+    region
+except NameError:
+    region = 'US'
+
+if region != 'EU':
+    print("You are using the US data center")
+    driver = webdriver.Remote(
+        command_executor='https://'+os.environ['SAUCE_USERNAME']+':'+os.environ['SAUCE_ACCESS_KEY']+'@ondemand.us-west-1.saucelabs.com/wd/hub',
+        desired_capabilities=sauceParameters)
+elif region == 'EU':
+    print ("You are using the EU data center")
+    driver = webdriver.Remote(
+        command_executor='https://'+os.environ['SAUCE_USERNAME']+':'+os.environ['SAUCE_ACCESS_KEY']+'@ondemand.eu-central-1.saucelabs.com:443/wd/hub',
+        desired_capabilities=sauceParameters)
+
+###################################################################
+# Test logic goes here
+###################################################################
+# Navigating to a website
+#__________________________________________________________________
+driver.get('https://www.dryzz.com')
+
+# Setup for finding an element and clicking it
+#__________________________________________________________________
+interact = driver.find_element_by_id('menu-item-112')
+interact.click()
+
+# Setup for finding an element and sending keystrokes
+#__________________________________________________________________
+# interact = driver.find_element_by_class_name('figure')
+# interact.send_keys('Dryzz')
+# interact.submit()
+
+# Setup for using random Python commands
+#__________________________________________________________________
+# driver.save_screenshot('screenshot.png')
+# sleep(10)
+# print('Message')
+
+# Setup for using Action chains
+#__________________________________________________________________
+# ActionChains(driver).move_to_element(interact).perform()
+
+# Setup for random script executions
+#__________________________________________________________________
+# driver.execute_script('sauce: break')
+# driver.execute_script('sauce:context=Place words here for notes')
+
+
+# Ending the test session
+#__________________________________________________________________
+driver.quit()
+
+
