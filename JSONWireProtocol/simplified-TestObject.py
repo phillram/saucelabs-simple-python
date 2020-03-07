2a3e8d0a823fbc10c27643dac253b7077dc9dd14
unknown <14913130+phillram@users.noreply.github.com>
2020-03-07 08:50:57 -0800

    Clarified required vs. optional capabilities. Added dynamic allocation

--- JSONWireProtocol/simplified-TestObject.py
+++ JSONWireProtocol/simplified-TestObject.py
@@ -4,26 +4,23 @@
 # wait 30 seconds. Then quit test
 ####################################################################
 
-
 from appium import webdriver
 from time import sleep
 
 sauceParameters = {
-    'appiumVersion': '1.8.1',
-    'deviceName' : 'Google Pixel 2',
-    'deviceOrientation' : 'portrait',
-    'browserName' : 'Chrome',
-    'platformVersion' : '9',
-    'platformName' : 'Android',
     'testobject_api_key' : 'APIKEY', # Plug in your project API key here
+    'deviceName' : '.*Pixel.*', 
+    'platformName' : 'Android',
+    'browserName' : 'Chrome',
+    # The following are not required
+    # 'deviceOrientation' : 'portrait',
+    # 'platformVersion' : '10',
+    # 'appiumVersion': '1.16.0',
 }
-
-
 
 driver = webdriver.Remote(
     command_executor='https://us1.appium.testobject.com/wd/hub',
     desired_capabilities=sauceParameters)
-
 
 sleep(30)
 
